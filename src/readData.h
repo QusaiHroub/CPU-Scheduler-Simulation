fe53cd9f189809afb9aa7a914ee4bced6186a559
Mohammad-Abureesh <64136459+Mohammad-Abureesh@users.noreply.github.com>
2020-05-12 22:53:00 +0300

    Update readData.h

--- src/readData.h
+++ src/readData.h
@@ -1,3 +1,14 @@
+/*
+ * This file is part of OS Project.
+ *
+ * Authors
+ * Qusai Hroub
+ * Mohammad Abureesh
+ *
+ * C++ program for read text file.
+ * 
+ */
+
 #include <fstream>
 #include <string>
 #include <iostream>
@@ -7,21 +18,37 @@ using namespace std;
 
 class ReadData {
 private:
+    
+    //Attributes
     string m_data;
     ifstream m_inFile;
+    
+    //Declare an array of process, for using these processes in all algorithms in this program 
     Process m_processes[5];
+    
+    //To put all data in this array, when use function readFile().
     int m_tempArr[24];
+    
     int m_memSize;
     int m_pageSize;
+    
+    // quantum for round robin algorithm
     int m_quantum;
     int m_contextSwitch;
+    //end Attributes.
 
 public:
+    
+//Functions
+
     void openFile();
     void printFile();
     int readFile();
     void closeFile();
+    
+    //To return  array of Processes
     Process *getProcesses();
+    
     int getContextSwitch();
     int getQuantum();
     int getMemSize();
