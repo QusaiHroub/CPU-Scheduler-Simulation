c3f57264c3d6e618cf1cdb875a90ed9f1583a901
Mohammad-Abureesh <64136459+Mohammad-Abureesh@users.noreply.github.com>
2020-05-12 20:09:36 +0300

    Update sjf.hpp

--- src/sjf.hpp
+++ src/sjf.hpp
@@ -5,6 +5,9 @@
  * Mohammad Abureesh
  * Qusai Hroub
  *
+ * C++ program for implementation of SJF algorithm  
+ * scheduling
+ * this class inherits all characteristics in <ProcessManagement>
  */
 
 #include "process.h"
@@ -17,6 +20,7 @@ using namespace std;
 
 class SJF : public ProcessManagement{
 public:
+  //constructors 
     SJF(Process * = nullptr, int = 0, int = 5);
     ~SJF();
 
@@ -25,10 +29,13 @@ private:
     vector<Process> m_readyQueue;
     bool *visited;
 
+ //Functions 
+    
+    //this Function initializes all function for algorithm
     void init();
 
     void calcCompletionTime();
-
+    
     static bool comp(Process &, Process &);
     static bool compCPUBurst(Process &, Process &);
 };
