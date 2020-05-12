26c93fc4081d09d96365ab22895cff23c002d688
QusaiHroub <qusaihroub.r@gmail.com>
2020-05-12 16:42:47 +0300

    Fix Pager

--- src/pager.cpp
+++ src/pager.cpp
@@ -12,6 +12,9 @@ Pager::~Pager() {
     if (m_memMap.first.first) {
         delete (m_memMap.first.first);
     }
+    if (m_memMap.second) {
+        delete m_memMap.second;
+    }
 }
 
 void Pager::init(int memSize, int fSize) {
@@ -23,6 +26,13 @@ void Pager::init(int memSize, int fSize) {
         m_memMap.first.first = nullptr;
     }
     m_memMap.first.first = (bool *) calloc(m_memMap.first.second, sizeof(bool));
+    if (m_memMap.second) {
+        delete m_memMap.second;
+    }
+    m_memMap.second = new pair<int, int>[m_memMap.first.second];
+    for (int i = 0; i < m_memMap.first.second; i++) {
+        m_memMap.second[i] = pair <int, int> (-1, -1);
+    }
 }
 
 pair<PageTable **, int> Pager::paging(Process * processes, int len) {
@@ -32,4 +42,8 @@ pair<PageTable **, int> Pager::paging(Process * processes, int len) {
     }
 
     return pair<PageTable **, int> (list, len);
+}
+
+pair < pair < bool *, int >, pair< int, int > *> &Pager::getMemMap() {
+    return m_memMap;
 }
--- src/pager.hpp
+++ src/pager.hpp
@@ -7,8 +7,8 @@
 class Pager {
     int m_memSize;
     int m_fSize;
-    pair < pair < bool *, int >, pair< int, int > > m_memMap =
-            pair < pair < bool *, int >, pair< int, int > >(pair < bool *, int > (nullptr, 0), pair< int, int > (-1, -1));
+    pair < pair < bool *, int >, pair< int, int > *> m_memMap =
+            pair < pair < bool *, int >, pair< int, int > *>(pair < bool *, int > (nullptr, 0), nullptr);
 public:
     Pager();
     Pager(int, int);
@@ -16,6 +16,8 @@ public:
 
     void init(int, int);
     pair<PageTable **, int> paging(Process *, int);
+
+    pair < pair < bool *, int >, pair< int, int > *> & getMemMap();
 };
 
 #endif // PAGER_HPP
