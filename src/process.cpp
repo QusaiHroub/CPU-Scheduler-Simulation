6bbecdbdd83f20b570932bcf276c54b2d1f63e0d
Mohammad-Abureesh <64136459+Mohammad-Abureesh@users.noreply.github.com>
2020-05-12 22:35:50 +0300

    Update process.cpp

--- src/process.cpp
+++ src/process.cpp
@@ -1,5 +1,21 @@
-#include "process.h"
+/*
+ * This file is part of OS Project.
+ *
+ * Authors
+ * Qusai Hroub
+ * Mohammad Abureesh
+ *
+ * C++ Program
+ * this file, declaration for "process.hpp"
+ * 
+ */
 
+#include "process.hpp"
+
+/*
+This constructor receives process ID, arrival time, CPU burst, and size for each process.
+size in bytes for each process, and Passing these values via <setters> 
+*/
 Process::Process(int id, int arrivalTime, int cpuBurst, int size) {
     setID(id);
     setArrivalTime(arrivalTime);
@@ -7,34 +23,45 @@ Process::Process(int id, int arrivalTime, int cpuBurst, int size) {
     setSize(size);
 }
 
+//Default constructor
 Process::Process() {}
 
+//this function return id for process 
 int Process::getID() {
     return m_id;
 }
 
+//this function return Arrival time for process 
 int Process::getArrivalTime() {
     return m_arrivalTime;
 }
 
+//this function return CPU Burst for process 
 int Process::getCpuBurst() {
     return m_cpuBurst;
 }
 
+//this function return size for process 
 int Process::getSize() {
     return m_size;
 }
 
+//To change Process id
 void Process::setID(int id) {
     m_id = id;
 }
 
+//To change Process arrival time
 void Process::setArrivalTime(int arrivalTime) {
     m_arrivalTime = arrivalTime;
 }
+
+//To change Process CPU Burst
 void Process::setCPUBurst(int cpuBurst) {
     m_cpuBurst = cpuBurst;
 }
+
+//To change Process Size
 void Process::setSize(int size) {
     m_size = size;
 }
