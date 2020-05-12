3633618ab72b66b69be39c189bd0ca0aa4729031
Mohammad-Abureesh <64136459+Mohammad-Abureesh@users.noreply.github.com>
2020-05-12 22:22:03 +0300

    Update process.h

--- src/process.h
+++ src/process.h
@@ -1,18 +1,38 @@
+/*
+ * This file is part of OS Project.
+ *
+ * Authors
+ * Qusai Hroub
+ * Mohammad Abureesh
+ *
+ * C++ program for Create Process
+ * 
+ */
+
 #pragma once
 #include<iostream>
 using namespace std;
+
 class Process
 {
 private:
+
+    //Attributes for each process			
     int m_arrivalTime;
     int m_cpuBurst;
-    int m_size;   // size in Bytes
+	
+    // size in Bytes		
+    int m_size;   
     int m_id;
-	
+    //end attributes		
+
 public:
-	
+    
+    //Constructor 	 
     Process();
     Process(int, int, int, int);
+
+    //getters and setters
 	
     int getArrivalTime();
     int getCpuBurst();
