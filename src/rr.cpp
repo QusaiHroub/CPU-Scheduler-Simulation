a4ca07328b1b9e283d40619012c35220a68ca402
Mohammad-Abureesh <64136459+Mohammad-Abureesh@users.noreply.github.com>
2020-05-13 00:57:24 +0300

    Update rr.cpp

--- src/rr.cpp
+++ src/rr.cpp
@@ -1,46 +1,86 @@
+/*
+ * This file is part of OS Project.
+ *
+ * Authors
+ * Qusai Hroub
+ * Mohammad Abureesh
+ *
+ * C++ program for implementation of RR algorithm
+ * this file, declaration for "RR.hpp"
+ */
+
 #include "RR.hpp"
 
 #include <algorithm>
 #include <vector>
 
+/*
+This constructor receives an array of Processes, context Switch (CS), 
+the size of the operations and Quantum, and if the array equals nullptr, 
+it returns and does not implement the algorithm. Otherwise, it calls the function that implements the algorithm init().
+*/
 RR::RR(Process* processes, int cs, int processesSize, int quantum): ProcessManagement(processes, cs, processesSize) {
     if (processes == nullptr) {
         return;
     }
 
+    //call this function for implements RR algorithm
     init(quantum);
 }
 
+//deconstructor
 RR::~RR() {}
 
+ /*
+the comparison function.
+this function receives two processes.
+return true if arrival time for the first process less than arrival time for the second process.
+*/
 bool RR::comp(Process &p1, Process &p2) {
     return p1.getArrivalTime() < p2.getArrivalTime();
 }
 
+/*
+this function work on implementation <RR> algorithm
+receive one integer Parameter (quantum) for use in this algorithm.
+*/
 void RR::init(int quantum) {
     m_quantum = quantum;
+    
+    
     Process *processes = getProcesses();
 
+    //Arrange the Processes based on the arrival time before starting the calculations
     sort(processes, processes + getProcessesSize(), comp);
 
+    //these functions are defined in the <ProcessManagement> class. 
     calcCompletionTime();
     calcTurnAroundTime();
     calcWaitingTime();
 
+   //As long as this variable equals true means that the algorithm has been implemented
     is_init = true;
 }
 
+// Function to calculate Completion time
 void RR::calcCompletionTime() {
     int len = getProcessesSize();
+    
+    //Declare an array to store completion time for each process
     int *completionTime = new int[len];
+    
     Process *processes = getProcesses();
 
     vector< pair <string, int> > timeLine;
 
+    //Declare an array to store CPU Burst for each process
     int cpuBurst[len];
+    
     int lastTime = 0;
     int maxIndex = 0;
     vector<int> queue;
+    
+    //Declare array to check if Process in Queue or not 
     bool isInQ[len];
     for (int i = 1; i < len; i++) {
         isInQ[i] = false;
@@ -51,6 +91,7 @@ void RR::calcCompletionTime() {
     int totalOverhead = 0;
     int maxCompletionTime = lastTime;
 
+    //To add in array <cpuBurst>
     for (int i = 0; i < len; i++) {
         cpuBurst[i] = processes[i].getCpuBurst();
     }
@@ -59,15 +100,26 @@ void RR::calcCompletionTime() {
         timeLine.push_back(pair<string, int>("  ", processes->getArrivalTime()));
     }
 
+    //Start loop
     loop:
+    
+    // if Queue is not Empty enter into to loop
     while (!queue.empty()) {
         int index = queue.front();
+        
+        /*
+           If burst time is smaller than or equal to 
+           quantum. Last cycle for this process 
+        */
         if (m_quantum < cpuBurst[index]) {
             if (getCS())
                 timeLine.push_back(pair<string, int>("CS", lastTime + getCS()));
 
             lastTime += m_quantum + getCS();
+            
+            //Reduce the CPU Burst for process via subtracting quantum in each cycle
             cpuBurst[index] -= m_quantum;
+            
             totalOverhead += getCS();
 
             timeLine.push_back(pair<string, int>("P" + to_string(processes[index].getID()), lastTime));
@@ -78,6 +130,8 @@ void RR::calcCompletionTime() {
             lastTime += cpuBurst[index] + getCS();
             cpuBurst[index] = 0;
             completionTime[index] = lastTime;
+            
+            //change max Completion time
             if (lastTime > maxCompletionTime) {
                 maxCompletionTime = lastTime;
             }
@@ -107,6 +161,7 @@ void RR::calcCompletionTime() {
             lastTime = processes[queue.front()].getArrivalTime();
             timeLine.push_back(pair<string, int>("  ", lastTime));
             goto loop;
+            //end loop
         }
 
     }
