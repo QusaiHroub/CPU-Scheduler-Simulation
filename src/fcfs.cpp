1360ada3142e4138aa99a6fd0ba305bdc9d5ba17
Mohammad-Abureesh <64136459+Mohammad-Abureesh@users.noreply.github.com>
2020-05-12 04:17:17 +0300

    Update fcfs.cpp

--- src/fcfs.cpp
+++ src/fcfs.cpp
@@ -5,36 +5,51 @@
  * Qusai Hroub
  * Mohammad Abureesh
  *
+ * C++ program for implementation of FCFS algorithm
+ * this file, declaration for "FCFS.hpp"
  */
 
-#include <algorithm>
 
+#include <algorithm>
 #include "fcfs.hpp"
 
+/*
+This constructor receives an array of Processes, context Switch (CS), 
+and the size of the operations, and if the array equals nullptr, 
+it returns and does not implement the algorithm. Otherwise, it calls the function that implements the algorithm init().
+*/
 FCFS::FCFS(Process *processes, int cs, int processesSize): ProcessManagement(processes, cs, processesSize) {
     if (processes == nullptr) {
         return;
     }
 
-    init();
+    init(); //call this function for implements FCFS algorithm
 }
 
+//deconstructor
 FCFS::~FCFS() {
-
 }
 
+//this function return true, when the arrival time of the first process is less than the arrival time of the second process.
 bool FCFS::comp(Process &p1, Process &p2) {
     return p1.getArrivalTime() < p2.getArrivalTime();
 }
 
+//this function work on implementation <FCFS> algorithm 
 void FCFS::init() {
-    Process *processes = getProcesses();
+    
+    Process *processes = getProcesses(); //getProcesse() function defined in the <ProcessManagement> class return array of Processes
 
+    
+    //Arrange the Processes based on the arrival time before starting the calculations
     sort(processes, processes + getProcessesSize(), comp);
 
-    calcCompletionTime();
-    calcTurnAroundTime();
-    calcWaitingTime();
-
+ //these functions are defined in the <ProcessManagement> class.   
+    
+    calcCompletionTime();//to find Completion time for each process
+    calcTurnAroundTime();//to find Arround time for each process
+    calcWaitingTime();//to find waiting time for each process
+    
+    //As long as this variable equals true means that the algorithm has been implemented
     is_init = true;
 }
