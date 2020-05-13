558e1733cd46126736bdbd7cc07188e08adad0d7
Mohammad-Abureesh <64136459+Mohammad-Abureesh@users.noreply.github.com>
2020-05-13 06:22:17 +0300

    Update pagetable.hpp

--- src/pagetable.hpp
+++ src/pagetable.hpp
@@ -1,11 +1,22 @@
+/*
+ * This file is part of OS Project.
+ *
+ * Authors
+ * Qusai Hroub
+ * Mohammad Abureesh
+ *
+ */
+
 #ifndef PAGETABLE_HPP
 #define PAGETABLE_HPP
 
-#include "process.h"
+#include "process.hpp"
 
 class PageTable {
 
 public:
+    
+    //constructor
     PageTable(Process * = nullptr, int = 128, pair<bool *, int> = pair<bool *, int> (nullptr, 0));
     ~PageTable();
 
@@ -15,6 +26,7 @@ public:
         bool isInMem = 0;
     };
 
+    //functions
     int getLength();
     int getPgaeSize();
     int getProcessID();
@@ -22,13 +34,20 @@ public:
     tableElement *getDeepCopyOfTable();
 
 private:
+    
+    //Attributes
     int m_length;
     int m_pageSize;
     tableElement *m_table;
     Process *m_process;
     pair<bool *, int> m_memoryMap;
 
+    //functions
+    
+    //To implements PagerTable
     void init();
+    
+    //To change Frame size (page size)
     void setPageSize(int);
 };
 
